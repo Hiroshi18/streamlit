6792cb37328810e18b807c152ae6bf8958516ed8
Lucas Hiroshi Horinouchi <lucas.hh@hotmail.com>
2025-03-12 09:48:27 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,13 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
+
+spell = st.secrets['spell']
+key = st.secrets.some_magic_api.key
+
+st.write(key)
+st.write(spell)
+
 st.markdown("""# This is a header
 ## This is a sub header
 This is text""")
